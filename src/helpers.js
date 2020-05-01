bb42ac5ae43c07493f896e4359c08a557f6b57a3
Leah Migacz <lmigacz@msudenver.edu>
2020-04-30 21:40:55 -0600

    API key woohoo

--- src/helpers.js
+++ src/helpers.js
@@ -1,2 +1,10 @@
 export const formatDate = (date) => 
 date.toLocalDateDtring(undefined, { day: 'numeric', month: 'long' });
+
+export const formatTemperature = (temperature, isMetric) => {
+    let displayTemp = temperature;
+    if (!isMetric) {
+        returnTemp = (temperature - 32) * (5 / 9);
+    }
+    return Math.round(displayTemp);
+}
