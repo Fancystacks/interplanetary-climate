090dec9e81bd2a888195a4ac36f777a3acd37858
Leah Migacz <lmigacz@msudenver.edu>
2020-05-01 17:17:02 -0600

    rewrote url

--- src/api.js
+++ src/api.js
@@ -1,2 +1,2 @@
 const API_KEY = "9dAc7CifCLpk1j7m1dmFj80idWZeB4Tp16jKGden";
-export const API_URL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
+export const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
